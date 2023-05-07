aa9fc867ff75e2ab77d96a25caf4aaef03d882ee
Tobias Wolter <towo@towo.eu>
2023-05-07 17:37:03 +0200

    Modify backend host in entrypoint

---
+++ static/frontend/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ -n "${GROCY_DOCKER_BACKEND_HOST}" ]; then
+	sed -e "s/backend:9000/${GROCY_DOCKER_BACKEND_HOST}:9000/" /etc/nginx/common.conf > /tmp/common.conf; 
+	cat /tmp/common.conf > /etc/nginx/common.conf;
+fi
+
+exec nginx -e /proc/self/fd/2 -g 'daemon off;'
