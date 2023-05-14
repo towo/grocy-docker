5f2446f59c8fa310dbfa705edc135370c1ff73ef
Tobias Wolter <towo@towo.eu>
2023-05-14 13:27:53 +0200

    Run entrypoint as root because ugh

--- static/frontend/entrypoint.sh
+++ static/frontend/entrypoint.sh
@@ -3,4 +3,4 @@ if [ -n "$1" ]; then
 	sed -i -e "s/backend:9000/$1:9000/" /etc/nginx/common.conf;
 fi
 
-nginx -e /proc/self/fd/2 -g 'daemon off;'
+su -c "nginx -e /proc/self/fd/2 -g 'daemon off;'" nginx
