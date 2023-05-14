34f7ba4c09f17ed6211701585c2bcce348382f29
Tobias Wolter <towo@towo.eu>
2023-05-14 22:17:09 +0200

    Use an environment variable for the backend host

--- static/frontend/entrypoint.sh
+++ static/frontend/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if [ -n "$1" ]; then
-	sed -e "s/backend:9000/$1:9000/" /etc/nginx/common.conf > /tmp/common.conf; 
+if [ -n "${GROCY_DOCKER_BACKEND_HOST}" ]; then
+	sed -e "s/backend:9000/${GROCY_DOCKER_BACKEND_HOST}:9000/" /etc/nginx/common.conf > /tmp/common.conf; 
 	cat /tmp/common.conf > /etc/nginx/common.conf;
 fi
 
