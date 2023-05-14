89c49585e9c00fca2e0fef7787d3794d058795b3
Tobias Wolter <towo@towo.eu>
2023-05-14 17:32:51 +0200

    jfc

--- static/frontend/entrypoint.sh
+++ static/frontend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -n "$1" ]; then
 	sed -e "s/backend:9000/$1:9000/" /etc/nginx/common.conf > /tmp/common.conf; 
-	cp /tmp/common.conf /etc/nginx/common.conf;
+	cat /tmp/common.conf > /etc/nginx/common.conf;
 fi
 
 nginx -e /proc/self/fd/2 -g 'daemon off;'
